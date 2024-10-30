ee00e8c29d073ca1f4abd4a379dccae8b8b8bc35
Aymenkenway <aymenkenway414@gmail.com>
2024-10-30 18:06:00 +0100

    Adjusted webpack config

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,36 +1,55 @@
 const path = require('path');
 
 module.exports = {
-	mode: 'production',
-	entry: {
-		main: path.resolve(__dirname, './js/dashboard_main.js'),
-	},
-	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js',
-	},
-	performance: {
-		maxAssetSize: 1000000,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(gif|svg|png|jpg|jpeg)$/i,
-				type: 'asset/resource',
-				use: [
-					{
-						loader: ['file-loader', 'image-webpack-loader'],
-						options: {
-							bypassOnDebug: true,
-							disable: true,
-						},
-					},
-				],
-			},
-		],
-	},
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    performance: {
+        maxAssetSize: 1000000, // Limits asset size warnings
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'], // CSS handling
+            },
+            {
+                test: /\.(gif|svg|png|jpg|jpeg)$/i,
+                type: 'asset/resource', // Built-in Webpack 5 asset module for images
+                generator: {
+                    filename: 'images/[name][ext]', // Output path for images
+                },
+                use: [
+                    {
+                        loader: 'image-webpack-loader', // Image optimization
+                        options: {
+                            disable: true, // Set to false to enable in production
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65,
+                            },
+                            optipng: {
+                                enabled: true,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
 };
